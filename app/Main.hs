c26fcafd2c5587404fefacd0688557e4ca5ec3a6
Aaron Ash <aaron.ash@gmail.com>
2015-07-17 15:41:56 +0800

    Initial hircules bot Just joins a channel and listens currently.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
 module Main where
 
-import Lib
+import Network
+import System.IO
+import Text.Printf
+
+{-import Lib-}
+
+server = "irc.freenode.org"
+port = 6667
+chan = "#hircules"
+nick = "hircules"
 
 main :: IO ()
-main = someFunc
+{-main = someFunc-}
+
+main = do
+  h <- connectTo server (PortNumber (fromIntegral port))
+  hSetBuffering h NoBuffering
+  write h "NICK" nick
+  write h "USER" (nick++" 0 * :hircules bot")
+  write h "JOIN" chan
+  listen h
+
+write :: Handle -> String -> String -> IO ()
+write h s t = do
+  hPrintf h "%s %s\r\n" s t
+  printf "%s %s\r\n" s t
+
+listen :: Handle -> IO ()
+listen h = forever $ do
+  s <- hGetLine h
+  putStrLn s
+ where
+    forever a = do a; forever a
