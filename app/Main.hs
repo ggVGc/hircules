8f9e34315a1fd76a7d87c1b82f2cdd90529af697
Aaron Ash <aaron.ash@gmail.com>
2015-07-19 00:52:44 +0800

    Added url title lookups

--- app/Main.hs
+++ app/Main.hs
@@ -8,17 +8,15 @@ import Control.Monad.Reader
 import Network
 import System.Exit
 import System.IO
+import System.Time
 import Text.Printf
 
-{-import Lib-}
+import Lib
 
 server = "irc.freenode.org"
 port = 6667
 chan = "#hircules"
 nick = "hircules"
-
-data Bot = Bot { socket :: Handle}
-type Net = ReaderT Bot IO
 
 main :: IO ()
 main = bracket connect disconnect loop
@@ -28,9 +26,10 @@ main = bracket connect disconnect loop
 
 connect :: IO Bot
 connect = notify $ do
+  t <- getClockTime
   h <- connectTo server (PortNumber (fromIntegral port))
   hSetBuffering h NoBuffering
-  return (Bot h)
+  return (Bot h chan t)
  where
   notify = bracket_
       (printf "Connecting to %s ... " server >> hFlush stdout)
@@ -47,23 +46,24 @@ listen :: Handle -> Net ()
 listen h = forever $ do
   s <- init `fmap` liftIO (hGetLine h)
   liftIO (putStrLn s)
-  if ping s then pong s else eval (clean s)
+  if ping s 
+  then pong s 
+  else when (isprivmsg s) $
+            eval (clean s)
  where
     clean = drop 1 . dropWhile (/= ':') . drop 1
+    isprivmsg = isPrefixOf "PRIVMSG" . drop 1 . dropWhile (/= ' ') . drop 1 
     ping x = "PING :" `isPrefixOf` x
     pong x = write "PONG" (':' : drop 6 x)
 
+notCommand :: String -> Bool
+notCommand = not . isPrefixOf "!" 
+
 eval :: String -> Net ()
-eval "!quit"                   = write "QUIT" ":Exiting" >> liftIO exitSuccess
-eval x | "!id " `isPrefixOf` x = privmsg (drop 4 x)
-eval _                         = return ()
-
-privmsg :: String -> Net ()
-privmsg s = write "PRIVMSG" (chan ++ " :" ++ s)
-
-write :: String -> String -> Net ()
-write s t = do
-  h <- asks socket
-  liftIO $ hPrintf h "%s %s\r\n" s t
-  liftIO $ printf "%s %s\r\n" s t
+eval "!quit"                    = write "QUIT" ":Exiting" >> liftIO exitSuccess
+eval "!uptime"                  = uptime >>= privmsg
+eval x | "!id " `isPrefixOf` x  = privmsg (drop 4 x)
+eval x | notCommand x
+       , hasURLs x              = lookupURLTitles x
+eval _                          = return ()
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,68 @@
 module Lib
-    ( someFunc
+    ( Bot (..),
+      Net,
+      lookupURLTitles,
+      hasURLs,
+      privmsg,
+      uptime,
+      write
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc LOLO"
+import Control.Arrow
+import Control.Exception
+import Control.Monad (forever)
+import Control.Monad.Reader
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import Network
+import System.IO
+import System.Time
+import Text.HTML.Scalpel
+import Text.Printf
+
+data Bot = Bot { socket :: Handle, channell :: String, starttime :: ClockTime }
+type Net = ReaderT Bot IO
+
+privmsg :: String -> Net ()
+privmsg s = do
+  chan<- asks channell
+  write "PRIVMSG" (chan++ " :" ++ s)
+
+write :: String -> String -> Net ()
+write s t = do
+  h <- asks socket
+  liftIO $ hPrintf h "%s %s\r\n" s t
+  liftIO $ printf "%s %s\r\n" s t
+
+uptime :: Net String
+uptime = do
+  now <- liftIO getClockTime
+  zero <- asks starttime
+  return . pretty $ diffClockTimes now zero
+
+pretty :: TimeDiff -> String
+pretty td =
+  unwords $ map (uncurry (++) . first show) $ 
+  if null diffs then [(0,"s")] else diffs
+    where merge (tot,acc) (sec,typ) = let (sec',tot') = divMod tot sec
+                                      in (tot',(sec',typ):acc)
+          metrics = [(86400,"d"),(3600,"h"),(60,"m"),(1,"s")]
+          diffs = filter ((/= 0) . fst) $ reverse $ snd $
+                  foldl' merge (tdSec td,[]) metrics
+
+hasURLs :: String -> Bool
+hasURLs s = "http://" `isInfixOf` s || "https://" `isInfixOf` s 
+
+lookupURLTitles :: String -> Net ()
+lookupURLTitles s = do
+  titles <- liftIO $ mapM scrapeTitle urls
+  mapM_ privmsg $ catMaybes titles
+ where
+    urls = filter isURL words
+    words = splitOn " " s
+    isURL s = "http://" `isPrefixOf` s ||
+              "https://" `isPrefixOf` s
+
+scrapeTitle :: String -> IO (Maybe String)
+scrapeTitle u = scrapeURL u (text "title")
